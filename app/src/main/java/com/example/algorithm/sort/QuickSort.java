d5e0db7b5c390b2d6980e4f1f3253fe97328ec91
wanghailong <wanghailong>
2021-08-24 18:46:00 +0800

    change:添加注释

--- app/src/main/java/com/example/algorithm/sort/QuickSort.java
+++ app/src/main/java/com/example/algorithm/sort/QuickSort.java
@@ -3,7 +3,7 @@ package com.example.algorithm.sort;
 import java.util.Arrays;
 
 /**
- * 快速排序
+ * 快速排序 分治思想 将原始数据分为两部分
  * 时间复杂度O(nlogn)
  * 原生排序算法
  * 非稳定排序算法，数值相等的数据在排序后位置可能会交换或改变
@@ -12,8 +12,8 @@ public class QuickSort {
 
 
     public static void main(String[] args) {
-//        int a[] = {3, 2, 1, 4, 7, 9};
-        int a[] = {1, 2, 3, 4, 5, 6};
+        int a[] = {3, 2, 1, 4, 7, 9};
+//        int a[] = {1, 2, 3, 4, 5, 6};
         System.out.println("排序前：" + Arrays.toString(a));
         quickSort(a, a.length);
         System.out.println("排序后：" + Arrays.toString(a));
@@ -26,14 +26,26 @@ public class QuickSort {
 
     // 快速排序递归函数，p起始下标,r为结束下标
     private static void quickSortInternally(int[] a, int p, int r) {
-        System.out.println("p=" + p + " r=" + r);
         if (p >= r) return;
 
         int q = partition(a, p, r); // 获取分区点
+        System.out.println("p=" + p + " r=" + (q - 1));
+        System.out.println("执行了前半部分！！！");
         quickSortInternally(a, p, q - 1);
+
+        System.out.println("q=" + (q + 1) + " r=" + r);
         quickSortInternally(a, q + 1, r);
+        System.out.println("执行了后半部分！！！");
     }
 
+    /**
+     * 查找分区点
+     *
+     * @param a
+     * @param p
+     * @param r
+     * @return
+     */
     private static int partition(int[] a, int p, int r) {
         int pivot = a[r];
         int i = p;
